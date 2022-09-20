8cdc99ef9ef42b797788ff23b26b214149c1ce9b
tshrivastava <80909551+tshrivastav@users.noreply.github.com>
2022-09-21 03:26:47 +0530

    make hero section responsive and added icond in search button

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,20 @@
 import React from "react";
-
+import { AiOutlineRight } from "react-icons/ai";
 const Search = () => {
   return (
     <div>
       <p className="text-white text-center">
         Ready to watch? Enter your email to create or restart your membership.
       </p>
-      <div className="flex flex-row items-center justify-center w-full mt-5">
+      <div className="flex flex-col gap-2 md:gap-0 md:flex-row items-center justify-center w-full mt-5">
         <input
-          className="p-4 flex-1 outline-none text-gray-600"
+          className="w-full p-4 flex-1 outline-none text-gray-600"
           type="email"
           placeholder="Enter your Email"
         />
-        <button className="text-white bg-myRed border-none px-6 py-4">
+        <button className="text-white bg-myRed border-none px-6 py-4 flex flex-row items-center gap-2">
           Get Started
+          <AiOutlineRight />
         </button>
       </div>
     </div>
